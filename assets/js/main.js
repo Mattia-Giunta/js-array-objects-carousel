31571f243ec797e360a629ed56ee734c3aac66dd
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-01-22 20:12:17 +0100

    sviluppato lista di immagini da cliccare sotto al carosello e terminato esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,7 @@ const arrowLeft = document.getElementById('arrowPrev');
 const placeHTML = document.getElementById('place');
 const infoHTML = document.getElementById('info');
 const figureHtml = document.getElementById('figure');
+const listIMG = document.getElementById('listImage');
 
 console.log( figureHtml )
 
@@ -40,7 +41,7 @@ const images = [
     
     {
         place:'Stati Uniti',
-        info:'antelope canion',
+        info:'Antelope Canion',
         link:'./assets/img/antelope canyon.jpg',
     },
     {
@@ -69,18 +70,44 @@ const images = [
 // variabile per dare una posizione all'immagine
 let currentImage = 0;
 
+// array vuoto per le immagini sotto al carosello
+let tumbnailsArray = [];
+
+// variabile per creare un div contenitore delle immagini sotto al carosello nell'Html
+let tumbContainer;
+
 // assegna la prima immagine e il contenuto di pertenza
 figureHtml.setAttribute('src', images[currentImage].link);
 placeHTML.innerText = images[currentImage].place;
 infoHTML.innerText = images[currentImage].info;
 
 
-
+// ciclo per creare le immagini sotto al carosello
 for(let i = 0; i < images.length; i++){
 
+    // creazione del div, assegnazione dello stile, pusharlo nell'array e inserimento dell'elemento con append 
+    tumbContainer = document.createElement('div')
+    tumbContainer.style.backgroundImage = `url("${images[i].link}")`;
+    tumbContainer.classList.add('tumbIMG')
+    tumbnailsArray.push(tumbContainer)
+    listIMG.append(tumbContainer)
+
+
+    // evento al click per selezionare l'immagine sotto e farla comparire nel carosello
+    tumbContainer.addEventListener('click', function(){
+
+        currentImage = tumbnailsArray.indexOf(this);
+
+        figureHtml.setAttribute('src', images[currentImage].link);
+        placeHTML.innerText = images[currentImage].place;
+        infoHTML.innerText = images[currentImage].info;
+
+        console.log(tumbnailsArray.indexOf(this))
+    })
 
 }
 
+console.log(tumbnailsArray)
 
 // al click fa scorrete le immagini a destra
 arrowRight.addEventListener('click', function(){
@@ -119,3 +146,5 @@ arrowLeft.addEventListener('click', function(){
     infoHTML.innerText = images[currentImage].info;
 
 })
+
+// Terminato esercizio
